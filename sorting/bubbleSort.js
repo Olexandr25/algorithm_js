56a21d37004151a7a39b1c521119baed010fce2f
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2022-10-01 14:33:33 +0300

    Fixed bubbleSort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,17 +1,16 @@
-let arr = [1,5,29,0,1,-5,-17,55,29,95,-98,-9,1,5,15];
-let arr2 = [30, 0, -5, 101, -500, 404];
-let count = 0;
+let arr = [1, 5, 29, 0, 1, -5, -17, 55, 29, 95, -98, -9, 1, 5, 15]
+let arr2 = [30, 0, -5, 101, -500, 404]
+let count = 0
 
-const bubbleSort = (arr) => {
- for (let i = 0; i < arr.length; i++) {
-  for (let j = 0; j < arr.length; j++) {
-    if (arr[j + 1] < arr[j]){
-      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+const bubbleSort = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j + 1] < arr[j]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
     }
-    count++;
   }
- }
- return arr
+  return arr
 }
 
 console.log(bubbleSort(arr2))
--- test.js
+++ test.js
@@ -1,37 +1,106 @@
-class KnapsackItem {
-  /**
-   * @param {Object} itemSettings - knapsack item settings,
-   * @param {number} itemSettings.value - value of the item.
-   * @param {number} itemSettings.weight - weight of the item.
-   * @param {number} itemSettings.itemsInStock - how many items are available to be added.
-   */
-  constructor({ value, weight, itemsInStock = 1 }) {
-    this.value = value
-    this.weight = weight
-    this.itemsInStock = itemsInStock
-    // Actual number of items that is going to be added to knapsack.
-    this.quantity = 1
-  }
+// let arrayToSearch = [2, 6, 8, 12, 43, 78, 99, 134, 144, 156, 199, 256, 500]
 
-  get totalValue() {
-    return this.value * this.quantity
-  }
+// console.log('Found at position :' + jumpSearch(arrayToSearch, 2))
+// console.log('Found at position :' + jumpSearch(arrayToSearch, 256))
+// console.log('Found at position :' + jumpSearch(arrayToSearch, 500))
+// console.log('Found at position :' + jumpSearch(arrayToSearch, 44444))
 
-  get totalWeight() {
-    return this.weight * this.quantity
-  }
+// function jumpSearch(arrayToSearch, valueToSearch) {
+//   let length = arrayToSearch.length
+//   let step = Math.floor(Math.sqrt(length))
+//   let lowerBound = 0
 
-  // This coefficient shows how valuable the 1 unit of weight is
-  // for current item.
-  get valuePerWeightRatio() {
-    return this.value / this.weight
-  }
+//   while (arrayToSearch[Math.min(step, length) - 1] < valueToSearch) {
+//     lowerBound = step
+//     step += step
+//     if (lowerBound >= length) {
+//       return -1
+//     }
+//   }
 
-  toString() {
-    return `v${this.value} w${this.weight} x ${this.quantity}`
+//   let upperBound = Math.min(step, length)
+
+//   while (arrayToSearch[lowerBound] < valueToSearch) {
+//     lowerBound++
+//     if (lowerBound == upperBound) {
+//       return -1
+//     }
+//   }
+//   if (arrayToSearch[lowerBound] == valueToSearch) {
+//     return lowerBound
+//   }
+//   return -1
+// }
+// ----------------------------------------------------------------
+// !
+
+let arr = [1, 5, 29, 0, 1, -5, -17, 55, 29, 95, -98, -9, 1, 5, 15]
+let arr2 = [30, 0, -5, 101, -500, 404]
+
+const bubbleSort = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j + 1] < arr[j]) {
+        ;[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
+      }
+    }
   }
+  return arr
 }
 
+// console.log(bubbleSort(arr))
+// ----------------------------------------------------------------
+// !
 
-const knapsack = new KnapsackItem({  value: 5, weight: 2, itemsInStock: 2  })
-console.log(knapsack)
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
+    ;[arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+  }
+  return arr
+}
+// console.log(selectionSort(arr))
+// ----------------------------------------------------------------
+// !
+
+const insertionSort = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    let prevElement = arr[i]
+    let nextElement = i - 1
+
+    while (nextElement >= 0 && arr[nextElement] > prevElement) {
+      arr[nextElement + 1] = arr[nextElement]
+      nextElement--
+    }
+
+    arr[nextElement + 1] = prevElement
+  }
+
+  return arr
+}
+// console.log(insertionSort(arr))
+// ----------------------------------------------------------------
+// !
+
+const bs = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    let value = arr[i]
+    let j = i - 1
+
+    while (j >= 0 && arr[j] > value) {
+      arr[j + 1] = arr[j]
+      j--
+    }
+
+    arr[j + 1] = value
+  }
+
+  return arr
+}
+
+console.log(bubbleSort(arr))
